f79073ba93fb62f97274be318691a7eb8db03c7f
Admin <mdoeden@unomaha.edu>
2022-05-02 14:11:54 -0500

    Moving on to basic GUI.

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 from tkinter import *
+import csv
 
 
 class GUI:
     def __init__(self, window):
         self.window = window
-
+        # Frame for STUDENT input.
         self.frame_name = Frame(self.window)
         self.label_name = Label(self.frame_name, text='Student: ')
         self.entry_name = Entry(self.frame_name)
@@ -12,4 +13,11 @@ class GUI:
         self.entry_name.pack(padx=5, side='left')
         self.frame_name.pack(anchor='w', pady=10)
 
+        # Frame for SCORE input.
+        self.frame_score = Frame(self.window)
+        self.label_score = Label(self.frame_score, text='Score: ')
+        self.entry_score = Entry(self.frame_score)
+        self.label_score.pack(padx=5, side='left')
+        self.entry_score.pack(padx=5, side='left')
+        self.frame_score.pack(anchor='w', pady=10)
 
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from grade import *
+from gui import *
 
 
 def main():
-    input_loop()
+    window = Tk()
+    window.title('Lab 10')  # Change the window title to 'Lab 10'.
+    window.geometry('250x180')  # Set its length to 250 and height to 180.
+    # window.resizable(False, False)  # Make the window non-resizable.
+    widgets = GUI(window)
+    window.mainloop()
 
 
 if __name__ == '__main__':
